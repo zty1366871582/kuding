163d8b1086c3c5c19a15cdbfa66f8b9cea2d3a13
ljm <794384313@qq.com>
2018-09-13 18:23:42 +0800

    first commit

---
+++ src/main/java/com/qilinxx/kuding/domain/mapper/DetailMapper.java
@@ -0,0 +1,10 @@
+package com.qilinxx.kuding.domain.mapper;
+
+import com.qilinxx.kuding.domain.model.Detail;
+import org.springframework.stereotype.Repository;
+import tk.mybatis.mapper.common.Mapper;
+
+@Repository
+public interface DetailMapper extends Mapper<Detail>{
+
+}
